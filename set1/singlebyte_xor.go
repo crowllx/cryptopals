46f2f181e44ea776759e4f39cd2d3514bb481d04
crowll <114693487+crowllx@users.noreply.github.com>
2024-07-12 08:22:03 -0700

    update set 1 challenge 3

--- set1/singlebyte_xor.go
+++ set1/singlebyte_xor.go
@@ -11,6 +11,38 @@ func xor(input []byte, c byte) []byte {
 	}
 	return xor
 }
+
+func scoreChar(char byte) int {
+    char_scores := map[byte]int{
+        byte('e'): 13,
+        byte('a'): 13,
+        byte('r'): 12,
+        byte('i'): 12,
+        byte('o'): 11,
+        byte('t'): 11,
+        byte('n'): 10,
+        byte('s'): 10,
+        byte('l'): 9,
+        byte('c'): 9,
+        byte('u'): 8,
+        byte('d'): 8,
+        byte('p'): 7,
+        byte('m'): 7,
+        byte('h'): 6,
+        byte('g'): 6,
+        byte('b'): 5,
+        byte('f'): 5,
+        byte('y'): 4,
+        byte('w'): 4,
+        byte('k'): 3,
+        byte('v'): 3,
+        byte('x'): 2,
+        byte('z'): 2,
+        byte('j'): 1,
+        byte('q'): 1,
+    }
+    return char_scores[char]
+}
 func SingleByteXor(input string) ([]rune, []string) {
 	var xors []string
 	var keys []rune
