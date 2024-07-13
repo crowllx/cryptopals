448aba95e2f6aaa9e007b3bd207c00336900a296
crowllx <114693487+crowllx@users.noreply.github.com>
2024-07-13 11:48:00 -0700

    add functions for single char xor detection

--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"cryptopals/set1"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	result := set1.SingleByteXor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-    for _, s := range result {
-        fmt.Println(s)
-    }
+	key, xor := set1.SingleByteXor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	fmt.Println("char: ", key, "\nxor:", xor)
+
+	dat, err := os.ReadFile("./tests/single-char-xor.txt")
+	if err != nil {
+		panic(err)
+	}
+	inputs := strings.Split(string(dat), "\n")
+
+	var xors []string
+	for _, s := range inputs {
+		_, xor = set1.SingleByteXor(s)
+		xors = append(xors, xor)
+	}
+	best := set1.ScoreList(xors)
+	fmt.Println(best)
 }
--- set1/singlebyte_xor.go
+++ set1/singlebyte_xor.go
@@ -2,6 +2,7 @@ package set1
 
 import (
 	"encoding/hex"
+	"strings"
 )
 
 func xor(input []byte, c byte) []byte {
@@ -12,46 +13,70 @@ func xor(input []byte, c byte) []byte {
 	return xor
 }
 
-func scoreChar(char byte) int {
-    char_scores := map[byte]int{
-        byte('e'): 13,
-        byte('a'): 13,
-        byte('r'): 12,
-        byte('i'): 12,
-        byte('o'): 11,
-        byte('t'): 11,
-        byte('n'): 10,
-        byte('s'): 10,
-        byte('l'): 9,
-        byte('c'): 9,
-        byte('u'): 8,
-        byte('d'): 8,
-        byte('p'): 7,
-        byte('m'): 7,
-        byte('h'): 6,
-        byte('g'): 6,
-        byte('b'): 5,
-        byte('f'): 5,
-        byte('y'): 4,
-        byte('w'): 4,
-        byte('k'): 3,
-        byte('v'): 3,
-        byte('x'): 2,
-        byte('z'): 2,
-        byte('j'): 1,
-        byte('q'): 1,
-    }
-    return char_scores[char]
+func scoreXor(xor []byte) int {
+	char_scores := map[byte]int{
+		byte('e'): 13,
+		byte('a'): 13,
+		byte('r'): 12,
+		byte('i'): 12,
+		byte('o'): 11,
+		byte('t'): 11,
+		byte('n'): 10,
+		byte('s'): 10,
+		byte('l'): 9,
+		byte('c'): 9,
+		byte('u'): 8,
+		byte('d'): 8,
+		byte('p'): 7,
+		byte('m'): 7,
+		byte('h'): 6,
+		byte('g'): 6,
+		byte('b'): 5,
+		byte('f'): 5,
+		byte('y'): 4,
+		byte('w'): 4,
+		byte('k'): 3,
+		byte('v'): 3,
+		byte('x'): 2,
+		byte('z'): 2,
+		byte('j'): 1,
+		byte('q'): 1,
+	}
+	score := 0
+	xor = []byte(strings.ToLower(string(xor)))
+	for _, b := range xor {
+		score += char_scores[b]
+	}
+	return score
 }
-func SingleByteXor(input string) ([]rune, []string) {
-	var xors []string
-	var keys []rune
+func ScoreList(strs []string) string {
+	best := 0
+	var best_str string
+	for _, s := range strs {
+		score := scoreXor([]byte(s))
+		if score > best {
+			best = score
+			best_str = s
+		}
+
+	}
+	return best_str
+}
+func SingleByteXor(input string) (key byte, output string) {
+
+	best_score := 0
+	best_xor := new(string)
+	var best_key byte
 
 	hex, _ := hex.DecodeString(input)
-	for c := 'a'; c < 'z'; c++ {
-		xors = append(xors, string(xor(hex, byte(c))))
-		keys = append(keys, c)
+	for c := 0; c < 255; c++ {
+		xor := xor(hex, byte(c))
+		score := scoreXor(xor)
+		if score > best_score {
+			best_score = score
+			*best_xor = string(xor)
+			best_key = byte(c)
+		}
 	}
-
-	return result
+	return best_key, *best_xor
 }
